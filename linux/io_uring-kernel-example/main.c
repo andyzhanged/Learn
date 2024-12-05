39c182441b5487ff855f629cfa70b13a0a9b9f9e
erdong.zhang <erdong.zhang@iluvatar.com>
2024-12-05 14:16:23 +0800

    add iouring example which test pass on linux 6.8.0-49
    Signed-off-by: erdong.zhang <erdong.zhang@iluvatar.com>

---
+++ linux/io_uring-kernel-example/main.c
@@ -0,0 +1,140 @@
+// open
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// atoi
+#include <stdlib.h>
+
+// pwritev
+#include <sys/uio.h>
+
+// fprintf
+#include <stdio.h>
+
+// close
+#include <unistd.h>
+
+// memset
+#include <string.h>
+
+// io_uring
+#include <liburing.h>
+#define QD 2
+
+int sync_rw(int fd, struct iovec *iov, int iovcnt, int offset)
+{
+  size_t count;
+
+  fprintf(stderr, "Performing sync RW\n");
+
+  count = preadv(fd, iov, iovcnt, offset);
+  if (count < 0) {
+    fprintf(stderr, "Unable to preadv in /dev/rw_iter\n");
+    return -1;
+  }
+
+  count = pwritev(fd, iov, iovcnt, offset);
+  if (count < 0) {
+    fprintf(stderr, "Unable to pwritev in /dev/rw_iter\n");
+    return -1;
+  }
+}
+
+int async_rw(int fd, struct iovec *iov, int iovcnt, int offset)
+{
+  struct io_uring_sqe *sqe;
+  struct io_uring_cqe *cqe;
+  struct io_uring ring;
+  static int const_read = 1, const_write = 2;
+  int ret;
+
+  fprintf(stderr, "Performing async RW\n");
+
+  ret = io_uring_queue_init(QD, &ring, 0);
+  if (ret < 0) {
+    fprintf(stderr, "queue_init: %s\n", strerror(-ret));
+    return -1;
+  }
+
+  sqe = io_uring_get_sqe(&ring);
+  if (sqe == NULL) {
+    fprintf(stderr, "Can't get sqe\n");
+    return -1;
+  }
+  io_uring_prep_readv(sqe, fd, iov, iovcnt, offset);  
+  io_uring_sqe_set_data(sqe, &const_read);
+
+  sqe = io_uring_get_sqe(&ring);
+  if (sqe == NULL) {
+    fprintf(stderr, "Can't get sqe\n");
+    return -1;
+  }
+  io_uring_prep_writev(sqe, fd, iov, iovcnt, offset);  
+  io_uring_sqe_set_data(sqe, &const_write);
+
+  if (io_uring_submit(&ring) != 2) {
+    fprintf(stderr, "Can't do submit\n");
+    return -1;
+  }
+
+  for (int i = 0; i < 2; i++) {
+    if (io_uring_wait_cqe_nr(&ring, &cqe, 1) < 0) {
+      fprintf(stderr, "Can't wait cqe\n");
+      return -1;
+    }
+
+    if (io_uring_cqe_get_data(cqe) == &const_write)
+      fprintf(stderr, "Write completed (%d bytes)\n", cqe->res);
+
+    if (io_uring_cqe_get_data(cqe) == &const_read)
+      fprintf(stderr, "Read completed (%d bytes)\n", cqe->res);
+
+    io_uring_cqe_seen(&ring, cqe);
+  }
+
+  io_uring_queue_exit(&ring);
+}
+
+int main(int argc, const char *argv[]) {
+  int iovcnt = 4;
+  int block_size = 512;
+  char *buffer[iovcnt];
+  void *cbuffer;
+  struct iovec iov[iovcnt];
+  int fd, i;
+
+  if (posix_memalign(&cbuffer, block_size, iovcnt * block_size) < 0) {
+    fprintf(stderr, "Unable to posix_memalign\n");
+    return -1;
+  }
+
+  memset(cbuffer, 1, iovcnt * block_size);
+  for (i = 0; i < iovcnt; i++) {
+    buffer[i] = cbuffer;
+    cbuffer += block_size;
+  }
+
+  fd = open("/dev/rw_iter", O_RDWR);
+  if (fd < 0) {
+    fprintf(stderr, "Unable to open /dev/rw_iter\n");
+    return -1;
+  }
+
+  for (i = 0; i < iovcnt; i++) {
+    printf("buffer[%d] at %p\n", i, buffer[i]);
+    sprintf(buffer[i], "Elias-%d\n", i);
+    iov[i].iov_base = buffer[i];
+    iov[i].iov_len = block_size;
+  }
+
+  if (argc == 2 && atoi(argv[1]))
+    async_rw(fd, iov, iovcnt, 0);
+  else
+    sync_rw(fd, iov, iovcnt, 0);
+
+  close(fd);
+
+  return 0;
+}
+
---
+++ linux/io_uring-kernel-example/rw_iter.c
@@ -0,0 +1,358 @@
+#include <linux/fs.h>
+#include <linux/kernel.h>
+#include <linux/miscdevice.h>
+#include <linux/mm.h>
+#include <linux/module.h>
+#include <linux/pagemap.h>
+#include <linux/uio.h>
+#include <linux/workqueue.h>
+#include <asm-generic/io.h>
+
+static char dummy_data[4800];
+
+struct workqueue_struct *workqueue;
+
+#define RW_MAX_PAGES 10
+struct rw_work {
+  struct delayed_work delayed_work;
+  struct kiocb *iocb;
+  int num_pages;
+  struct rw_page {
+    struct page *page;
+    size_t offs;
+    size_t bytes;
+    size_t seek;
+  } page [RW_MAX_PAGES];
+};
+
+static int sample_open(struct inode *inode, struct file *file) {
+  pr_info("I have been awoken\n");
+  return 0;
+}
+
+static int sample_close(struct inode *inodep, struct file *filp) {
+  pr_info("Sleepy time\n");
+  return 0;
+}
+
+static void describe(const struct iov_iter *iov_iter) {
+  int i;
+  pr_info("offset:%ld\n", iov_iter->iov_offset);
+  pr_info("count:%ld\n", iov_iter_count(iov_iter));
+  switch (iov_iter_rw(iov_iter)) {
+    case WRITE:
+      pr_info("direction:Write\n");
+      break;
+    case READ:
+      pr_info("direction:Read\n");
+      break;
+  }
+  switch (iov_iter_type(iov_iter)) {
+    case ITER_IOVEC:
+      pr_info("\tITER_IOVEC\n");
+      for (i = 0; i < iov_iter->nr_segs; i++) {
+        pr_info("\tbase:%pS\tlen=%ld\n", iov_iter->__iov[i].iov_base,
+                iov_iter->__iov[i].iov_len);
+      }
+      break;
+    case ITER_KVEC:
+      pr_info("ITER_KVEC\n");
+      break;
+    case ITER_UBUF:
+      pr_info("ITER_UBUF\n");
+      break;
+    case ITER_BVEC:
+      pr_info("ITER_BVEC\n");
+      break;
+    case ITER_DISCARD:
+      pr_info("ITER_DISCARD\n");
+      break;
+    case ITER_XARRAY:
+      pr_info("ITER_XARRAY\n");
+      break;
+    default:
+      pr_info("Other\n");
+      break;
+  }
+  pr_info("nr_segs:%ld\n", iov_iter->nr_segs);
+}
+
+// This function is inspired on block/bio.c:bio_map_user_iov
+static void inspect_pages(struct iov_iter *iov_iter) {
+  int num_pages;
+  struct page **pages;
+  const void *to_free;
+  struct iov_iter clone;
+  size_t len;
+  int npages;
+
+  // iov_iter_get_pages_alloc return a mapping to the first iov only
+  // if we want to map all of then, we going to need to interate over it
+  // So we don't have an array of num_pages after the return of
+  // iov_iter_get_pages_alloc as I tought it would be on the first place
+  num_pages = iov_iter_npages(iov_iter, INT_MAX);
+  pr_info("Total of pages:%d\n", num_pages);
+
+  // We will clone it because we want to inspect the memory without affect the
+  // iterator There is no documentation but the return of dup_ite must be free
+  // by the user
+  to_free = dup_iter(&clone, iov_iter, GFP_KERNEL);
+
+  // TODO: Remove the following lines, once we know what they do
+  // iov_iter_single_seg_count(&clone);
+  // length;
+
+  len = iov_iter_count(&clone);
+  pr_info("Total len=%ld\n", len);
+
+  for (int i = 0; i < clone.nr_segs; i++)
+  {
+    printk("iovec[%d] base addr %pS len %ld\n", i, clone.__iov[i].iov_base, clone.__iov[i].iov_len);
+  }
+
+  while (len > 0) {
+    int i;
+    size_t offs, bytes;
+    char us[32] = {
+        0,
+    };
+    int us_len = 0;
+    // We are going to use maxsize = PAGE_SIZE, it means it will return at most
+    // two pages
+    bytes = iov_iter_get_pages_alloc2(&clone, &pages, PAGE_SIZE, &offs);
+    pr_info("\tpage size bytes:%ld\n", bytes);
+    pr_info("\tpage offs:%ld\n", offs);
+
+    // We update how many bytes left to be mapped and advance the iterator
+    len -= bytes;
+
+    // To be sure the actual number of pages,
+    // we must calculate if the data crossed a page bondary
+    npages = DIV_ROUND_UP(offs + bytes, PAGE_SIZE);
+    pr_info("\tnumber of pages for this mapping:%d\n", npages);
+
+    for (i = 0; i < npages; i++) {
+      struct page *page = pages[i];
+      size_t n = PAGE_SIZE - offs;
+      void *myaddr;
+
+      if (n > bytes) n = bytes;
+
+      pr_info("\t\t%ld bytes on the page %d\n", n, i);
+
+      // kmap configures the MMU so we can access the memory from this context
+      myaddr = kmap(page) + offs;
+
+      // As you will se the addr are not continuos here like they are on the userspace
+      pr_info("\t\tkernel addr:%pS to %pS\n", myaddr, myaddr + n - 1);
+      pr_info("\t\tphysical addr:%lx to %lx\n",  virt_to_phys(myaddr), virt_to_phys(myaddr + n - 1));
+
+      offs = 0;
+      bytes -= n;
+
+      // Let's inspect the memory
+      for (; (us_len < sizeof(us) - 1) && n; us_len++, n--) {
+        us[us_len] = *((char *)myaddr);
+        myaddr++;
+      }
+
+      kunmap(page);
+    }
+
+    pr_info("\t\tuser string:%6s\n", us);
+    kvfree(pages);
+  }
+
+  kfree(to_free);
+}
+
+static void rw_work_fill_pages(struct rw_work *rw_work, struct iov_iter *iov_iter)
+{
+  size_t len;
+  size_t seek = iov_iter->iov_offset;
+
+  rw_work->num_pages = 0;
+  len = iov_iter_count(iov_iter);
+
+  while (len > 0) {
+    struct rw_page *rw_page = &rw_work->page[rw_work->num_pages];
+
+    rw_page->bytes = iov_iter_get_pages2(iov_iter, &rw_page->page, PAGE_SIZE, 1, &rw_page->offs);
+    len -= rw_page->bytes;
+    rw_page->seek = seek;
+    seek += rw_page->bytes;
+
+    rw_work->num_pages++;
+    if (rw_work->num_pages == RW_MAX_PAGES) {
+      pr_info("Not enough number of pages - truncating operation\n.");
+      break;
+    }
+  }
+  pr_info("got %d pages\n", rw_work->num_pages);
+}
+
+static void complete_read(struct work_struct *work)
+{
+  struct rw_work *rw_work = container_of(work, struct rw_work, delayed_work.work);
+  struct kiocb *iocb = rw_work->iocb;
+  size_t count = 0;
+
+  int i;
+  for (i = 0; i < rw_work->num_pages; i++) {
+    struct rw_page *rw_page = &rw_work->page[i];
+    void *kaddr = kmap(rw_page->page) + rw_page->offs;
+    ssize_t bytes = rw_page->bytes;
+    if ((bytes + rw_page->seek) > sizeof(dummy_data)) {
+      bytes = sizeof(dummy_data) - rw_page->seek;
+      bytes = (bytes < 0) ? 0 : bytes;
+    }
+    pr_info("Copy %ld (%ld) bytes from %pS from %ld\n", rw_page->bytes, bytes, kaddr, rw_page->seek);
+    memcpy(kaddr, dummy_data + rw_page->seek, bytes);
+    count += bytes;
+    if (kaddr)
+      kunmap(rw_page->page);
+    put_page(rw_page->page);
+  }
+
+  pr_info("delayed work %s\n", __func__);
+  if (iocb && iocb->ki_complete) {
+    iocb->ki_complete(iocb, count);
+    pr_info("delayed work called\n");
+  }
+  kfree(rw_work);
+}
+
+static ssize_t schedule_read_work(struct kiocb *iocb, struct iov_iter *to)
+{
+    struct rw_work *rw_work;
+    rw_work = kzalloc(sizeof(*rw_work), GFP_KERNEL);
+    rw_work->iocb = iocb;
+    rw_work_fill_pages(rw_work, to);
+    INIT_DELAYED_WORK(&rw_work->delayed_work, complete_read);
+    queue_delayed_work(workqueue, &rw_work->delayed_work, msecs_to_jiffies(2000));
+    return -EIOCBQUEUED;
+}
+
+static ssize_t sample_read_iter(struct kiocb *iocb, struct iov_iter *to) {
+  size_t len = iov_iter_count(to);
+
+  describe(to);
+  inspect_pages(to);
+
+  pr_info("Ykukky - I just throw up %ld bytes\n", len);
+  if (is_sync_kiocb(iocb)) {
+    pr_info("Synchronous read request\n");
+    return copy_to_iter(dummy_data, sizeof(dummy_data), to);
+  } else {
+    pr_info("Asynchronous read request\n");
+    return schedule_read_work(iocb, to);
+  }
+}
+
+static void complete_write(struct work_struct *work)
+{
+  struct rw_work *rw_work = container_of(work, struct rw_work, delayed_work.work);
+  struct kiocb *iocb = rw_work->iocb;
+  size_t count = 0;
+  int i;
+
+  for (i = 0; i < rw_work->num_pages; i++) {
+    struct rw_page *rw_page = &rw_work->page[i];
+    void *kaddr = kmap(rw_page->page) + rw_page->offs;
+    ssize_t bytes = rw_page->bytes;
+    if ((bytes + rw_page->seek) > sizeof(dummy_data)) {
+      bytes = sizeof(dummy_data) - rw_page->seek;
+      bytes = (bytes < 0) ? 0 : bytes;
+    }
+    pr_info("Copy %ld (%ld) bytes from %pS to %ld\n", rw_page->bytes, bytes, kaddr, rw_page->seek);
+    memcpy(dummy_data + rw_page->seek, kaddr, bytes);
+    count += bytes;
+    if (kaddr)
+      kunmap(rw_page->page);
+    put_page(rw_page->page);
+  }
+
+  pr_info("delayed work %s\n", __func__);
+  if (iocb && iocb->ki_complete) {
+    iocb->ki_complete(iocb, count);
+    pr_info("delayed work called\n");
+  }
+  kfree(rw_work);
+}
+
+static ssize_t schedule_write_work(struct kiocb *iocb, struct iov_iter *from)
+{
+    struct rw_work *rw_work;
+    rw_work = kzalloc(sizeof(*rw_work), GFP_KERNEL);
+    rw_work->iocb = iocb;
+    rw_work_fill_pages(rw_work, from);
+    INIT_DELAYED_WORK(&rw_work->delayed_work, complete_write);
+    queue_delayed_work(workqueue, &rw_work->delayed_work, msecs_to_jiffies(1000));
+    return -EIOCBQUEUED;
+}
+
+
+static ssize_t sample_write_iter(struct kiocb *iocb, struct iov_iter *from) {
+  size_t len = iov_iter_count(from);
+
+  describe(from);
+  inspect_pages(from);
+
+  pr_info("Yummy - I just ate %ld bytes\n", len);
+
+  if (is_sync_kiocb(iocb)) {
+    pr_info("Synchronous write request\n");
+    return copy_from_iter(dummy_data, sizeof(dummy_data), from);
+  } else {
+    pr_info("Asynchronous write request\n");
+    return schedule_write_work(iocb, from);
+  }
+}
+
+static const struct file_operations sample_fops = {
+    .owner = THIS_MODULE,
+    .open = sample_open,
+    .read_iter = sample_read_iter,
+    .write_iter = sample_write_iter,
+    .release = sample_close,
+    .llseek = no_llseek,
+};
+
+struct miscdevice sample_device = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "rw_iter",
+    .fops = &sample_fops,
+};
+
+static int __init misc_init(void) {
+  int error;
+
+  workqueue = create_singlethread_workqueue("RW testing\n");
+  if (workqueue == NULL) {
+    pr_err("Unable to create an workqueue\n");
+    return -1;
+  }
+  memset(dummy_data, 'a', sizeof(dummy_data));
+  error = misc_register(&sample_device);
+  if (error) {
+    destroy_workqueue(workqueue);
+    pr_err("can't misc_register :(\n");
+    return error;
+  }
+
+  pr_info("I'm in\n");
+  return 0;
+}
+
+static void __exit misc_exit(void) {
+  misc_deregister(&sample_device);
+  flush_workqueue(workqueue);
+  destroy_workqueue(workqueue);
+  pr_info("I'm out\n");
+}
+
+module_init(misc_init) module_exit(misc_exit)
+
+MODULE_DESCRIPTION("Simple Misc Driver");
+MODULE_AUTHOR("Nick Glynn <n.s.glynn@gmail.com>");
+MODULE_LICENSE("GPL");
